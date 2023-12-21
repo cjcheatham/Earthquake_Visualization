dc3f1598f6d9dd6c301d4ff3c666a87943ae6c29
cjcheatham <cjcheatham0500@gmail.com>
2023-12-20 17:27:26 -0800

    Updated logic file.

---
+++ Earthquake_Visualization/static/js/logic_eqv.js
@@ -0,0 +1,102 @@
+const earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(earthquake_url).then(function (data) {
+    createFeatures(data.features);
+});
+
+function createMarker(feature, latlng) {
+    return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color:"#000",
+        weight: 0.5,
+        opacity: 0.5,
+        fillOpacity: 1
+    });
+};
+
+function createFeatures (earthquakeData) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Location:</h3> ${feature.properties.place} <h3> Date:</h3> ${new Date(feature.properties.time)} <h3> Magnitude:</h3> ${feature.properties.mag}<h3> Depth:</h3> ${feature.geometry.coordinates[2]}`)
+    }
+
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: createMarker
+    });
+    
+    createMap(earthquakes)
+};
+
+
+function createMap(earthquakes) {
+    
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    let topo = L.tileLayer.wms('http://ows.mundialis.de/services/service?',{layers: 'TOPO-WMS'});
+
+    let sat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    });
+
+    let baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo,
+        "Satelite View": sat
+    };
+
+    let overlayMaps = {
+        Earthquakes: earthquakes
+    };
+
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 3,
+        layers: [street, topo, earthquakes]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+
+    let legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+    let div = L.DomUtil.create('div', 'info legend'),
+        grades = [-10, 10, 30, 60, 90],
+        labels = [],
+        legendInfo = "<h5>Magnitude</h5>";
+
+    for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + markerColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }    
+
+    return div;
+
+    };
+    legend.addTo(myMap);
+
+};
+
+
+function markerSize(magnitude) {
+    return magnitude * 5;
+};
+
+function markerColor(depth) {
+    return depth > 90 ? '#d73027' :
+            depth > 70 ? '#fc8d59' :
+            depth > 50 ? '#fee08b' :
+            depth > 30 ? '#d9ef8b' :
+            depth > 10 ? '#91cf60' :
+                         '#1a9850' ;          
+};
+
+
+
